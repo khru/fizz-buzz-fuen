953613dda3367b42d8079ce3aaf3d18120c6d866
FuenSansano <i.fuensanosano@gmail.com>
2022-05-07 18:42:21 +0200

    first failing test of converting 1 to 1

--- java/src/test/java/FizzBuzzTest.java
+++ java/src/test/java/FizzBuzzTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FizzBuzzTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void convert_1_to_1() {
+		assertEquals("1",new FizzBuzz().convert(1));
 	}
 
 }
