5a59395b528f657e1ceb90759af0e8c8cf68fa0d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-07 19:28:42 +0200

    refactor production code and test class, also added parametrization of the test

--- java/src/main/java/FizzBuzz.java
+++ java/src/main/java/FizzBuzz.java
@@ -1,12 +1,5 @@
 public class FizzBuzz {
   public static String convert(int givenNumber) {
-    if (givenNumber == 1) {
-      return "1";
-    }
-
-    if (givenNumber == 2) {
-      return "2";
-    }
-    return "4";
+    return String.valueOf(givenNumber);
   }
 }
---
+++ java/src/test/java/FizzBuzzShould.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FizzBuzzShould {
+
+  @ParameterizedTest
+  @CsvSource({
+    "1,1",
+    "2,2",
+    "4,4"
+  })
+  public void should_convert_a_number_not_divisible_by_three_nor_five_to_the_same_number_as_string(String expected, int givenNumber) {
+    assertEquals(expected,FizzBuzz.convert(givenNumber));
+  }
+}
