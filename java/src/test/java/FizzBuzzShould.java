e183b7fbb023346dd9338e926cf52b2c41bb3958
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-07 19:30:16 +0200

    added the convert_three_to_fizz use case

--- java/src/test/java/FizzBuzzShould.java
+++ java/src/test/java/FizzBuzzShould.java
@@ -16,4 +16,9 @@ public class FizzBuzzShould {
   public void should_convert_a_number_not_divisible_by_three_nor_five_to_the_same_number_as_string(String expected, int givenNumber) {
     assertEquals(expected,FizzBuzz.convert(givenNumber));
   }
+
+  @Test
+  public void convert_three_to_fizz() {
+    assertEquals("fizz",FizzBuzz.convert(3));
+  }
 }
