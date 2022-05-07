be4b23b87d7d4a47e6bc2a11954af0756f65b264
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-07 20:07:47 +0200

    making the convert_five_to_buzz use case pass

--- java/src/main/java/FizzBuzz.java
+++ java/src/main/java/FizzBuzz.java
@@ -1,5 +1,9 @@
 public class FizzBuzz {
   public static String convert(int givenNumber) {
+    if (givenNumber == 5) {
+      return "buzz";
+    }
+
     if (givenNumber % 3 == 0) {
       return "fizz";
     }
